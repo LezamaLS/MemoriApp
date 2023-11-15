5d71e82ff6666599d4ba0038e9744eaa9cd63720
Miguel Lezama <mikelezamaoro@gmail.com>
2023-11-14 22:17:07 -0600

    Initial Commit

---
+++ MemoriApp/MemoriAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriAppApp.swift
+//  MemoriApp
+//
+//  Created by Miguel Lezama on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoriAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
